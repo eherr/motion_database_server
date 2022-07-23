362aa01cf08d88532cb8d8d6be04dac941aa6480
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-07-23 03:41:43 +0200

    Added new method to export database to a folder

--- motion_database_server/motion_database.py
+++ motion_database_server/motion_database.py
@@ -26,7 +26,7 @@ import bson
 import bz2
 import base64
 import numpy as np
-from motion_database_server.utils import get_bvh_from_str, extract_compressed_bson, get_bvh_string
+from motion_database_server.utils import get_bvh_from_str, extract_compressed_bson, get_bvh_string, save_json_file
 from motion_database_server.user_database import UserDatabase
 from motion_database_server.skeleton_database import SkeletonDatabase
 from anim_utils.animation_data.skeleton_builder import SkeletonBuilder
@@ -176,6 +176,40 @@ class MotionDatabase(UserDatabase, SkeletonDatabase):
         self.connect_to_database(path)
         self.create_table(name, TABLES2[name], replace=True)
         
+    def export_database_to_folder(self, out_dir, parent_name=None):
+        os.makedirs(out_dir, exist_ok=True)
+        parent_id = 0
+        if parent_name is not None:
+            col = self.get_collection_by_name(parent_name)
+            if len(col) > 0:
+                parent_id = col[0][0]
+                print("parent", parent_id)
+        for skeleton_name in self.skeletons:
+            self.skeletons[skeleton_name] = self.load_skeleton(skeleton_name)
+            skeleton_data = self.skeletons[skeleton_name].to_unity_format()
+            skeleton_data["name"] = skeleton_name
+            save_json_file(skeleton_data, out_dir + os.sep+skeleton_name+".json")
+            col_out_dir = out_dir+os.sep+skeleton_name
+            self.export_motion_data(skeleton_name, col_out_dir+os.sep+"raw", parent=parent_id)
+            self.export_processed_motion_data(skeleton_name, col_out_dir+os.sep+"processed", parent=parent_id)
+            
+    def export_motion_data(self, skeleton_name, out_dir, parent=0):
+        for col in self.get_collection_list_by_id(parent):
+            print(col)
+            col_id, col_name, col_type, owner, public = col
+            action_dir = out_dir+os.sep+col_name
+            os.makedirs(action_dir, exist_ok=True)
+            self.export_collection_clips_to_folder(col_id, skeleton_name, action_dir)
+            self.export_motion_data(skeleton_name, action_dir, col_id)
+
+    def export_processed_motion_data(self, skeleton_name, out_dir, parent=0):
+        for col in self.get_collection_list_by_id(parent):
+            col_id, col_name, col_type, owner, public = col
+            action_dir = out_dir+os.sep+col_name
+            os.makedirs(action_dir, exist_ok=True)
+            self.export_processed_collection_data_to_folder(col_id, skeleton_name, action_dir)
+            self.export_processed_motion_data(skeleton_name, action_dir, col_id)
+
     def export_collection(self, path_str, skeleton_name, out_dir):
         paths = path_str.split("/")
         parent = 0
@@ -212,25 +246,84 @@ class MotionDatabase(UserDatabase, SkeletonDatabase):
         count = 1
         for motion_id, name in motion_list:
             print("download motion", str(count)+"/"+str(n_motions), name)
-            self.export_motion_clip(skeleton, motion_id, name, directory)
+            self.export_motion_clip(skeleton, motion_id, name, directory, export_annotation=True)
             count+=1
 
-    def export_motion_clip(self, skeleton, motion_id, name, directory):
+    def export_processed_collection_data_to_folder(self, c_id, skeleton_name, directory):
+        skeleton = self.load_skeleton(skeleton_name)
+        motion_list = self.get_preprocessed_data_list_by_collection(c_id, skeleton_name)
+        if motion_list is None:
+            print("could not find motions")
+            return
+        n_motions = len(motion_list)
+        if n_motions < 1:
+            print("no motions", c_id)
+            return
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+        count = 1
+        for motion_id, name in motion_list:
+            print("download motion", str(count)+"/"+str(n_motions), name)
+            self.export_preprocessed_data(skeleton, motion_id, name, directory, export_annotation=True)
+            count+=1
+
+
+    def export_motion_clip(self, skeleton, motion_id, name, directory, export_annotation=False):
         print("export clip")
-        data, skeleton, meta_data = self.get_motion_by_id(motion_id)
+        data, meta_data, skeleton_name = self.get_motion_by_id(motion_id)
         if data is None:
             return
         motion_dict = extract_compressed_bson(data)
-        print("write to file")
         motion_vector = MotionVector()
-        motion_vector.from_custom_unity_format(motion_dict)
+        motion_vector.from_custom_db_format(motion_dict)
+        #try:
         bvh_str = get_bvh_string(skeleton, motion_vector.frames)
         filename = directory+os.sep+name
         if not name.endswith(".bvh"):
             filename += ".bvh"
         with open(filename, "wt") as out_file:
             out_file.write(bvh_str)
-        
+        print("wrote file", filename)
+        #except Exception as e :
+        #    print("Error: writing file", motion_id, name, e.args)
+        #    pass
+        #    return
+        if export_annotation and meta_data is not None and meta_data != b"x00" and meta_data != "":
+            meta_filename = filename+"_meta.json"
+            try:
+                meta_data = bz2.decompress(meta_data)
+                meta_data = bson.loads(meta_data)
+                save_json_file(meta_data, meta_filename)
+            except:
+                print("Error could not decode",meta_data)
+                pass
+
+
+    def export_preprocessed_data(self, skeleton, motion_id, name, directory, export_annotation=False):
+        print("export clip")
+        data, meta_data, skeleton_name = self.get_preprocessed_data_by_id(motion_id)
+        if data is None:
+            return
+        motion_dict = extract_compressed_bson(data)
+        motion_vector = MotionVector()
+        motion_vector.from_custom_db_format(motion_dict)
+        bvh_str = get_bvh_string(skeleton, motion_vector.frames)
+        filename = directory+os.sep+name
+        if not name.endswith(".bvh"):
+            filename += ".bvh"
+        with open(filename, "wt") as out_file:
+            out_file.write(bvh_str)
+        print("wrote file", filename)
+        if export_annotation and meta_data is not None and meta_data != b"x00" and meta_data != "":
+            meta_filename = filename+"_meta.json"
+            try:
+                meta_data = bz2.decompress(meta_data)
+                meta_data = bson.loads(meta_data)
+                save_json_file(meta_data, meta_filename)
+            except:
+                print("Error could not decode",meta_data)
+                pass
+
     def import_database(self, other):
         skeletons = other.get_skeleton_list()
         for skeleton_id, skeleton_name in skeletons:
