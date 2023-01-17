4b1188ae4e9372569d1b88acebf68c87f44b939b
Erik Herrmann <erik.herrmann@dfki.de>
2023-01-17 17:00:31 +0100

    Adapted get_bvh_string() to new anim utils version

--- motion_database_server/utils.py
+++ motion_database_server/utils.py
@@ -3,7 +3,7 @@ import json
 import bz2
 import bson
 import numpy as np
-from anim_utils.animation_data.bvh import BVHReader, BVHWriter
+from anim_utils.animation_data.bvh import BVHReader, convert_quaternion_to_euler_frames, generate_bvh_string
 
 def save_json_file(data, file_path, indent=4):
     with open(file_path, "w") as out_file:
@@ -23,19 +23,19 @@ def get_bvh_from_str(bvh_str):
     return bvh_reader
 
 def get_bvh_string(skeleton, frames):
-    print("generate bvh string", len(skeleton.animated_joints))
-    frames = np.array(frames)
-    frames = skeleton.add_fixed_joint_parameters_to_motion(frames)
-    frame_time = skeleton.frame_time
-    bvh_writer = BVHWriter(None, skeleton, frames, frame_time, True)
-    return bvh_writer.generate_bvh_string()
+    print("generate bvh string", len(skeleton.animated_joints), skeleton.reference_frame_length, frames.shape)
+    if frames.shape[1] < skeleton.reference_frame_length:
+        frames = skeleton.add_fixed_joint_parameters_to_motion(frames)
+        print("after",  frames.shape)
+    euler_frames = convert_quaternion_to_euler_frames(skeleton, frames)
+    return generate_bvh_string(skeleton, euler_frames, skeleton.frame_time)
 
 
 def extract_compressed_bson(data):
     try:
         data = bson.loads(bz2.decompress(data))
     except:
-        data = bson.loads(data)
         print("Warning: data was not compressed")
+        data = bson.loads(data)
         pass
     return data
