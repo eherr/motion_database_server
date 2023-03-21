1dad88b4517742225890b7b609fb56728eeacb8c
Erik Herrmann <erik.herrmann@dfki.de>
2023-03-21 15:39:57 +0100

    Fixed error in database creation script

--- create_database.py
+++ create_database.py
@@ -25,7 +25,6 @@ from motion_database_server.schema import DBSchema, TABLES
 from motion_database_server.project_database import ProjectDatabase
 from motion_database_server.utils import load_json_file
 
-
 def create_database(db_path, project_name, user_name, pw, email):
     schema = DBSchema(TABLES)
     schema.create_database(db_path)
@@ -33,6 +32,7 @@ def create_database(db_path, project_name, user_name, pw, email):
     project_db.connect_to_database(db_path)
     user_id = project_db.create_user(user_name, pw, email, "admin", [])
     project_db.create_project(project_name, user_id, True)
+    project_db.close()
     
 
 
@@ -40,11 +40,11 @@ CONFIG_FILE = "db_server_config.json"
 if __name__ == "__main__":
     config = load_json_file(CONFIG_FILE)
     parser = argparse.ArgumentParser(description='Create database.')
-    parser.add_argument('project_name', nargs='?', help='project_name')
-    parser.add_argument('user_name', nargs='?', help='user name')
-    parser.add_argument('pw', nargs='?', help='password')
-    parser.add_argument('email', nargs='?', help='email')
+    parser.add_argument('project_name', help='project_name')
+    parser.add_argument('user_name', help='user name')
+    parser.add_argument('pw', help='password')
+    parser.add_argument('email', help='email')
     args = parser.parse_args()
     kwargs = vars(args)
-    if args.db_path is not None and args.user_name is not None and args.pw is not None and args.email is not None:
+    if args.user_name is not None and args.pw is not None and args.email is not None:
         create_database(config["db_path"], **kwargs)
