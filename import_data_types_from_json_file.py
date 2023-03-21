c6935c543b3b656e9d9c88109734861e88a505a3
Erik Herrmann <erik.herrmann@dfki.de>
2023-03-21 15:13:32 +0100

    Added script to import data types and data transforms

---
+++ import_data_types_from_json_file.py
@@ -0,0 +1,44 @@
+
+import argparse
+from motion_database_server.schema import DBSchema, TABLES
+from motion_database_server.utils import load_json_file
+from motion_database_server.motion_file_database import MotionFileDatabase
+from motion_database_server.data_transform_database import DataTransformDatabase
+
+def import_data_types(db_path, data):
+    schema = DBSchema(TABLES)
+    motion_db = MotionFileDatabase(schema)
+    motion_db.connect_to_database(db_path)
+    for tag in data["tag_list"]:
+        motion_db.create_tag(tag)
+    for name in data["data_types"]:
+        motion_db.create_data_type(data["data_types"][name])
+        for tag in data["data_types"][name]["tags"]:
+            motion_db.add_data_type_tag(name, tag)
+    for name in data["data_types"]:
+        motion_db.create_data_loader(data["data_types"]["name"])
+    motion_db.close()
+
+    data_transform_db = DataTransformDatabase(schema)
+    data_transform_db.connect_to_database(db_path)
+    for name in data["data_transforms"]:
+        dt_idx = data_transform_db.create_data_transform(data["data_transforms"][name])
+        for input_t, is_collection in data["data_transforms"][name]["inputs"]:
+            dt_input = {"dataTransform": dt_idx, "inputType": input_t, "isCollection": is_collection}
+            data_transform_db.create_data_transform_input(dt_input)
+    data_transform_db.close()
+    return data
+
+
+
+CONFIG_FILE = "db_server_config.json"
+if __name__ == "__main__":
+    config = load_json_file(CONFIG_FILE)
+    db_path = config["db_path"]
+    parser = argparse.ArgumentParser(description='Import data types to db.')
+    parser.add_argument('filename', help='Filename')
+    args = parser.parse_args()
+    if args.filename is not None:
+        data_types = load_json_file(args.filename)
+        import_data_types(db_path, data_types)
+    
